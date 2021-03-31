c1e474a1cfd785de8fb0aa6dd0e58da166d9463a
Caio Oliveira <caiolv@me.com>
2021-03-31 18:34:25 -0300

    add: adicionar tema para o react-native-paper

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { NativeRouter, Link, Route } from 'react-router-native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, AppRegistry } from 'react-native';
-import { FAB, Portal, Provider } from 'react-native-paper';
+import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
 // Import Views
 import LoginView from "./views/LoginView";
@@ -10,25 +10,43 @@ import PasswordView from "./views/PasswordView";
 import SignUpView from "./views/SignUpView";
 import HomeView from "./views/HomeView";
 
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: '#000099',
+    // accent: '#f1c40f',
+    // background: '', // background color for pages, such as lists.
+    // surface: '', //background color for elements containing content, such as cards
+    // text: '', //text color for content
+    //disabled: '', //color for disabled elements
+    //placeholder: '', //color for placeholder text, such as input placeholder.
+    //backdrop: '', //color for backdrops of various components such as modals
+  },
+};
+
 function App() {
   return (
-    <NativeRouter>
-      <View style={styles.container}>
-        <Route exact path="/">
-          <LoginView />
-        </Route>
-        <Route exact path="/signup">
-          <SignUpView />
-        </Route>
-        <Route exact path="/password">
-          <PasswordView />
-        </Route>
-        <Route exact path="/home">
-          <HomeView />
-        </Route>
-      </View>
-      <StatusBar style="auto" />
-    </NativeRouter>
+    <PaperProvider theme={theme}>
+      <NativeRouter>
+        <View style={styles.container}>
+          <Route exact path="/">
+            <LoginView />
+          </Route>
+          <Route exact path="/signup">
+            <SignUpView />
+          </Route>
+          <Route exact path="/password">
+            <PasswordView />
+          </Route>
+          <Route exact path="/home">
+            <HomeView />
+          </Route>
+        </View>
+        <StatusBar style="auto" />
+      </NativeRouter>
+    </PaperProvider>
   );
 }
 
