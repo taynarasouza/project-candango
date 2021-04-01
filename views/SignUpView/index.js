59900b0260c27df9e091b70656cb6c887246a878
Caio Oliveira <caiolv@me.com>
2021-04-01 19:58:03 -0300

    add: iniciar testes de validacao

--- views/SignUpView/index.js
+++ views/SignUpView/index.js
@@ -1,44 +1,83 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-native';
 import {
   View,
+  SafeAreaView,
   StyleSheet,
   TextInput,
   Image,
   KeyboardAvoidingView,
   Platform,
-  ScrollView
+  Alert
 } from 'react-native';
 import { Appbar, Avatar, HelperText } from 'react-native-paper';
+import * as Yup from 'yup';
 
 import Button from "../../components/Button";
 import { EmailField, PasswordField, PhoneInput, CustomInput } from "../../components/Fields";
 
 const SignUpView = () => {
   const history = useHistory();
-  const [name, setName] = React.useState("");
-  const [email, setEmail] = React.useState("");
-  const [phone, setPhone] = React.useState("");
-  const [senha, setSenha] = React.useState("");
+  const [values, setValues] = useState({
+    name: '',
+    email: '',
+    confirmEmail: '',
+    phone: '',
+    password: '',
+    confirmPassword: '',
+  });
 
-  const handleChangeEmail = email =>
-    setEmail(email);
+  const schema = Yup.object().shape({
+    name: Yup.string().required("Campo obrigatório."),
+    email: Yup.string()
+      .email("Email inválido.")
+      .required("Campo obrigatório."),
+    confirmEmail: Yup.string().when('email', (email, field) =>
+      email ? field.required().oneOf([Yup.ref('email')]) : field
+    ),
+    password: Yup.string()
+      .required("Campo obrigatório!")
+      .min(8, ({ min }) => `A senha deve ter pelo menos ${min} caracteres.`),
+    confirmPassword: Yup.string().when('password', (password, field) =>
+      password ? field.required().oneOf([Yup.ref('password')]) : field
+    ),
+  });
 
-  const handleChangeSenha = senha =>
-    setSenha(senha);
+  // const [name, setName] = React.useState("");
+  // const [email, setEmail] = React.useState("");
+  // const [phone, setPhone] = React.useState("");
+  // const [senha, setSenha] = React.useState("");
 
-  const handleChangeName = name => 
-    setName(name);
+  const handleChange = e => {
+    const { name, value } = e.currentTarget
+    setValues(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
 
-  const handleChangePhone = phone => 
-    setPhone(phone);
+  const handleSubmit = async() => {
+    if (!(await schema.isValid(values))) {
+      Alert.alert("Erro");
+    } else {
+      return 0;
+    }
+  }
+
+  // const handleChangeEmail = email =>
+  //   setEmail(email);
+
+  // const handleChangeSenha = senha =>
+  //   setSenha(senha);
+
+  // const handleChangeName = name => 
+  //   setName(name);
+
+  // const handleChangePhone = phone => 
+  //   setPhone(phone);
 
   const handleGoTo = path =>
     history.push(path);
-
-    const emailHasErrors = () => {
-      return !email.includes('@');
-    };
 
   return (
     <>
@@ -46,7 +85,7 @@ const SignUpView = () => {
         <Appbar.BackAction onPress={() => handleGoTo("/")} />
         <Appbar.Content title="Cadastro" />
       </Appbar.Header>
-      {/*<ScrollView>*/}
+      <SafeAreaView>
         <KeyboardAvoidingView
           behavior={Platform.OS === "ios" ? "padding" : "height"}
           style={styles.container}
@@ -54,20 +93,47 @@ const SignUpView = () => {
           <>
             <Avatar.Image size={200} source={require('../../assets/avatar_female.png')} />
             <View>
-              <CustomInput label="Nome" placeholder="Escreva seu nome" onChange={handleChangeName} />
-              <EmailField valeu={email} placeholder="Escreva seu email" onChange={handleChangeEmail} />
-              <HelperText type="error" visible={emailHasErrors()}>
-                Email inválido.
-              </HelperText>
-              <PhoneInput 
-                placeholder="(XX) X XXXX-XXXX" 
-                onChange={handleChangePhone} 
+              <CustomInput 
+                name="name"
+                label="Nome" 
+                placeholder="Escreva seu nome" 
+                onChange={handleChange} 
               />
-              <EmailField onChange={handleChangeEmail} />
-              <PasswordField onChange={handleChangeSenha} />
+              <EmailField 
+                name="email"
+                valeu={values.email} 
+                placeholder="Escreva seu email" 
+                onChange={handleChange} 
+              />
+              <EmailField 
+                name="confirmEmail"
+                label="Confirmar email"
+                valeu={values.email} 
+                placeholder="Escreva seu email" 
+                onChange={handleChange} 
+              />
+              {/*<HelperText type="error"> 
+               visible={emailHasErrors()}>
+                Email inválido.
+              </HelperText> */}
+              <PhoneInput 
+                value={values.phone}
+                placeholder="(XX) X XXXX-XXXX" 
+                onChange={handleChange} 
+              />
+              <PasswordField 
+                value={values.password} 
+                onChange={handleChange} 
+              />
+              <PasswordField 
+                label="Confirmar senha"
+                value={values.confirmPassword} 
+                onChange={handleChange} 
+              />
             </View>
             <View>
               <Button
+                onPress={handleSubmit}
                 variant="flat"
                 label="Cadastrar"
                 fullWidth
@@ -75,7 +141,7 @@ const SignUpView = () => {
             </View>
           </>
         </KeyboardAvoidingView>
-      {/*</ScrollView>*/}
+      </SafeAreaView>
     </>
   )
 };
