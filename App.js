ad90c2c81d6d3a4bd0d660fb2a85ae05653cd7e1
VijayKapoor92 <vijayk_1@hotmail.com>
2021-05-19 00:11:29 -0300

    refactor: Importa theme

--- App.js
+++ App.js
@@ -12,21 +12,7 @@ import SignUpView from "./src/views/SignUpView";
 import HomeView from "./src/views/HomeView";
 import NewPasswordView from './src/views/NewPasswordView';
 
-const theme = {
-  ...DefaultTheme,
-  roundness: 2,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: '#000099',
-    // accent: '#f1c40f',
-    // background: '', // background color for pages, such as lists.
-    // surface: '', //background color for elements containing content, such as cards
-    // text: '', //text color for content
-    //disabled: '', //color for disabled elements
-    //placeholder: '', //color for placeholder text, such as input placeholder.
-    //backdrop: '', //color for backdrops of various components such as modals
-  },
-};
+import { theme } from "./src/utils/theme";
 
 function App() {
   const [spinner, setSpinner] = useState(false);
