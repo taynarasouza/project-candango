985f71c337861f4588b507373699609e486ccd3f
VijayKapoor92 <vijayk_1@hotmail.com>
2021-05-19 00:12:24 -0300

    feat: Cria arquivo para otimizacao de import/export

---
+++ src/views/index.js
@@ -0,0 +1,17 @@
+import LoginView from "./LoginView";
+import HomeView from "./HomeView";
+import SignUpView from "./SignUpView";
+import PasswordView from "./PasswordView";
+import NewPasswordView from "./NewPasswordView";
+import ProfileView from "./ProfileView";
+import BagView from "./BagView";
+
+export {
+  LoginView,
+  SignUpView,
+  PasswordView,
+  NewPasswordView,
+  HomeView,
+  ProfileView,
+  BagView
+};
