6a374bf90916af2b54cbd20485ddac477aa64cb2
VijayKapoor92 <vijayk_1@hotmail.com>
2021-05-18 22:28:35 -0300

    feat: Cria arquivo para constantes

---
+++ src/utils/constants.js
@@ -0,0 +1,11 @@
+export const Routes = {
+  Login: "/",
+  SignUp: "/signup",
+  Password: "/password",
+  NewPassword: "/password/generate",
+  Home: "/home",
+  Profile: "/profile",
+  Bag: "/bag",
+  Medals: "/bag/medals",
+  Messages: "/bag/messages"
+}; 
