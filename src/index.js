8e1a87278009a62e0ec05406ed1bc2be6f553aad
VijayKapoor92 <vijayk_1@hotmail.com>
2021-06-07 22:17:15 -0300

    refactor: Remove o codigo nao utilizado

--- src/index.js
+++ src/index.js
@@ -4,34 +4,9 @@ import { useSelector } from 'react-redux';
 import createRouter from './routes';
 
 export default function Index() {
-    const [position, setPosition] = useState({
-        latitude: 0,
-        longitude: 0
-    });
-
-    useEffect(() => {
-        navigator.geolocation.getCurrentPosition(
-        position => {
-            console.log("--- My position ---");
-            console.log(position);
-            console.log("-------------------")
-            setPosition({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-            });
-        },
-        error => console.error(error),
-        { 
-            enableHighAccuracy: false, 
-            timeout: 200000, 
-            maximumAge: 1000 
-        },
-        );
-    }, []);
-
     const signed = useSelector(state => state.auth.signed);
 
-    const Routes = createRouter(signed, position);
+    const Routes = createRouter(signed);
 
     return <Routes  />;
 }
