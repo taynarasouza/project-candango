0820dcc8d8d4cfd2deb9f75c99ed9988ce476e1f
VijayKapoor92 <vijayk_1@hotmail.com>
2021-06-22 22:47:50 -0300

    feat: Implementa collapse

--- src/views/MarkerView/description.js
+++ src/views/MarkerView/description.js
@@ -1,17 +1,47 @@
 import React from "react";
 import { View, Text, StyleSheet, ScrollView } from "react-native";
 
+const MAX_HEIGHT = 216;
+const MAX_LINES = 10;
+
 const Description = ({description}) => {
+  const [show, setShow] = React.useState(false);
+  const [toggled, setToggled] = React.useState(false);
+  
+  const handleTextLayout = e => {
+    const shownText = e.nativeEvent.lines.slice(0, MAX_LINES).map((line) => line.text).join('');
+    const hiddenText = description.substring(shownText.length);
+    
+    if (hiddenText.length > 0)
+      setShow(true);
+  }
+
+  const handleToggle = () => {
+    setToggled(prevState => !prevState);
+  }
+
   return (
     <View style={styles.descriptionView}>
       <Text style={styles.label}>
         Sobre
       </Text>
-      <ScrollView style={styles.scroll}>
-        <Text style={styles.scrollText}>
+      <View>
+        <Text 
+          style={styles.scrollText}
+          numberOfLines={toggled ? undefined : MAX_LINES}
+          onTextLayout={handleTextLayout}
+        >
           {description}
         </Text>
-      </ScrollView>
+        {show && (
+          <Text 
+            onPress={handleToggle}
+            style={{color: "#000099"}}
+          >
+            Ver mais
+          </Text>
+        )}
+      </View>
     </View>
   )
 }
