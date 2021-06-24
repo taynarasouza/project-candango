2e0e8226238f10e513b5f4e3ebf61c263b51aa7d
VijayKapoor92 <vijayk_1@hotmail.com>
2021-06-23 23:11:06 -0300

    feat: Cria styled components

--- src/views/MarkerView/styles.js
+++ src/views/MarkerView/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
-import { Button as ButtonPaper } from 'react-native-paper';
+import { LinearGradient } from 'expo-linear-gradient';
+import { Button as ButtonPaper, Text } from 'react-native-paper';
 
 export const Button = styled(ButtonPaper).attrs(props => ({
   ...props
@@ -13,4 +14,35 @@ export const Button = styled(ButtonPaper).attrs(props => ({
 
   border-color: ${props => props.icon === "close" ? "rgb(255,0,0)" : "transparent"};
   border-width: ${props => props.icon === "close" ? "1px" : "0"};
+`;
+
+export const Gradient = styled(LinearGradient).attrs({
+  colors: ['transparent', 'rgba(0, 0, 0, 0.8)']
+})`
+  height: 100%;
+  justify-content: flex-end;
+`;
+
+export const Bold = styled(Text)`
+  color: white; 
+  font-weight: bold;
+  padding: 10px 15px;
+  font-size: 16px;
+`;
+
+export const Experience = styled.View`
+  padding: 5px;
+  padding-right: 15px;
+`;
+
+export const ExpLabel = styled(Text)`
+  font-weight: bold;
+  color: white;
+`;
+
+export const ExpValue = styled(Text)`
+  font-weight: bold;
+  font-size: 16px;
+  text-align: center;
+  color: white;
 `;
